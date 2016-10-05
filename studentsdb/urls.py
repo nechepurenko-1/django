806901bb87846dac3a8cc64f45903e06dfcbb488
Vladimir Nechepurenko <v.nechepurenko88@gmail.com>
2016-10-05 21:05:36 +0300

    back to django

--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -17,17 +17,19 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-                       url(r'^$','students.views.students_list',name='home'),
-                       url(r'^students/add/$','students.views.students_add',name='students_add'),
-                       url(r'^students/(?P<sid>\d+)/edit/$','students.views.students_edit',name='students_edit'),
-                       url(r'^students/(?P<sid>\d+)/delete/$','students.views.students_delete',name='students_delete'),
+                       url(r'^$','students.views.students.students_list',name='home'),
+                       url(r'^students/add/$','students.views.students.students_add',name='students_add'),
+                       url(r'^students/(?P<sid>\d+)/edit/$','students.views.students.students_edit',name='students_edit'),
+                       url(r'^students/(?P<sid>\d+)/delete/$','students.views.students.students_delete',name='students_delete'),
 
 
-					   url(r'^groups/$', 'students.views.groups_list', name='groups'),
-                       url(r'^groups/add/$','students.views.groups_add',name='groups_add'),
-                       url(r'^groups/(?P<gid>\d+)/edit/$','students.views.groups_edit',name='groups_edit'),
-                       url(r'^groups/(?P<gid>\d+)/delete/$','students.views.groups_delete',name='groups_delete'),
+					   url(r'^groups/$', 'students.views.groups.groups_list', name='groups'),
+                       url(r'^groups/add/$','students.views.groups.groups_add',name='groups_add'),
+                       url(r'^groups/(?P<gid>\d+)/edit/$','students.views.groups.groups_edit',name='groups_edit'),
+                       url(r'^groups/(?P<gid>\d+)/delete/$','students.views.groups.groups_delete',name='groups_delete'),
 
 					   url(r'^admin/', include(admin.site.urls)),
 
+                       url(r'^journal/$', 'students.views.journal.journal', name='journal'),
+                       url(r'^journal/(?P<gid>\d+)/edit/$', 'students.views.journal.journal_edit', name='journal_edit'),
                        )
