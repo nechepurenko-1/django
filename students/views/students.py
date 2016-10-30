efdfcceb3ed48f2b4a2d25b21e4d5e5599de6259
Vladimir Nechepurenko <v.nechepurenko88@gmail.com>
2016-10-30 16:57:17 +0200

    add student add form

--- students/views/students.py
+++ students/views/students.py
@@ -1,49 +1,121 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from ..models.students import Student
+from ..models.groups import Group
+from datetime import datetime
 
 
 def students_list(request):
-	students = Student.objects.all()
-	#try to order srudents list
-	order_by = request.GET.get('order_by','')
-	if order_by in ('last_name','first_name','ticket', 'id','student_group'):
-		students = students.order_by(order_by)
-		if request.GET.get('reverse', '') == '1':
-			students = students.reverse()
-	else:
-	    students = students.order_by('last_name')
+    students = Student.objects.all()
+    #try to order srudents list
+    order_by = request.GET.get('order_by','')
+    if order_by in ('last_name','first_name','ticket', 'id','student_group'):
+        students = students.order_by(order_by)
+        if request.GET.get('reverse', '') == '1':
+            students = students.reverse()
+    else:
+        students = students.order_by('last_name')
 
-	# paginate students
-	paginator = Paginator(students, 3)
-	page = request.GET.get('page')
+    # paginate students
+    paginator = Paginator(students, 3)
+    page = request.GET.get('page')
 
-	try:
-		students = paginator.page(page)
+    try:
+        students = paginator.page(page)
 
-	except PageNotAnInteger:
-		# if page not an integer, delliver first page
-		students = paginator.page(1)
+    except PageNotAnInteger:
+        # if page not an integer, delliver first page
+        students = paginator.page(1)
 
-	except EmptyPage:
-		#if page is out of range(e.g. 9999) , deliver last page of result
-		students = paginator.page(paginator.num_pages)
+    except EmptyPage:
+        #if page is out of range(e.g. 9999) , deliver last page of result
+        students = paginator.page(paginator.num_pages)
 
-	return render(request, 'students/students_list.html',
-		{'students': students})
+    return render(request, 'students/students_list.html',
+        {'students': students})
 
 def students_add(request):
-	return HttpResponse('<h1>Student Add Form</h1>')
+    # was form posted?
+    if request.method == "POST":
+        # was form add button clicked?
+        if request.POST.get('add_button') is not None:
+            # TODO: validate input from user
+            errors = {}
+            # validate student data will go here
+            data = {'middle_name': request.POST.get('middle_name'),'notes': request.POST.get('notes')}
+            # validate user input
+            first_name = request.POST.get('first_name', '').strip()
+            if not first_name:
+                errors['first_name'] = u"Ім`я є обов`язковим"
+            else:
+                data['first_name'] = first_name
+
+            last_name = request.POST.get('last_name', '').strip()
+            if not last_name:
+                errors['last_name'] = u"Прізвище є обов’язковим"
+            else:
+                data['last_name'] = last_name
+
+            birthday = request.POST.get('birthday', '').strip()
+            if not birthday:
+                errors['birthday'] = u"Дата народження є обов’язковою"
+            else:
+                try:
+                    datetime.strptime(birthday, '%Y-%m-%d')
+                except Exception:
+                    errors['birthday'] ='Введіть коректний формат дати(напр.1984-12-30)'
+                else:
+                    data['birthday'] = birthday
+
+            ticket = request.POST.get('ticket', '').strip()
+            if not ticket:
+                errors['ticket'] = u"Номер білета є обов’язковим"
+            else:
+                data['ticket'] = ticket
+
+            student_group = request.POST.get('student_group', "").strip()
+            if not student_group:
+                errors['student_group'] = u"Оберіть групу для студента"
+            else:
+                groups = Group.objects.filter(pk=student_group)
+                if len(groups) != 1:
+                    errors['student_group'] = u"Оберіть коректну групу"
+                else:
+                    data['student_group'] = groups[0]
+
+            photo = request.FILES.get('photo')
+            if photo:
+                data['photo'] = photo
+
+            if not errors:
+                # create student object
+                student = Student(**data)
+                # save it to database
+                student.save()
+                # redirect user to students list
+                return HttpResponseRedirect(reverse('home'))
+            else:
+                # render form with errors and previous user input
+                return render(request, 'students/students_add.html',
+                    {'groups': Group.objects.all().order_by('title'),'errors': errors})
+        elif request.POST.get('cancel_button') is not None:
+            # redirect to home page on cancel button
+            return HttpResponseRedirect(reverse('home'))
+    else:
+        # initial form render
+        return render(request, 'students/students_add.html',
+                        {'groups': Group.objects.all().order_by('title')})
 
 def students_edit(request, sid):
-	return render(request, 'students/student_edit.html')
+    return render(request, 'students/student_edit.html')
 
 
 # def students_edit(request, sid):
 # 	return HttpResponse('<h1>Edit Student %s</h1>' %sid)
 
 def students_delete(request, sid):
-	return HttpResponse('<h1>Delete Student %s</h1>' %sid)
+    return HttpResponse('<h1>Delete Student %s</h1>' %sid)
 
