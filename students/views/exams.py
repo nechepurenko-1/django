061dc42f5ac41796c6bbaf6e9a1fe662fb42cf9f
Vladimir Nechepurenko <v.nechepurenko88@gmail.com>
2016-12-02 22:50:13 +0200

    finish add form

--- students/views/exams.py
+++ students/views/exams.py
@@ -12,10 +12,11 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from crispy_forms.layout import Submit
 from django.contrib import messages
-
+from datetime import datetime
 
 
 from ..models.exams import Exam
+from ..models.groups import Group
 
 
 
@@ -100,13 +101,133 @@ class ExamsAddView(CreateView):
 
 
 def exams_add(request):
-	return HttpResponse('<h1>Exams Add Form</h1>')
+    if request.method == "POST":
+        if request.POST.get('add_button') is not None:
+            errors = {}
+            data = {}
+
+
+            group = request.POST.get('group', "").strip()
+            if group:
+                groups = Group.objects.filter(pk=int(group))
+                if len(groups) != 1:
+                    errors['group'] = u"Оберіть коректну групу"
+                else:
+                    data['group'] = groups[0]
+            else:
+                errors['group'] = u"Група є обов'язковою."
+
+            title = request.POST.get('title', '').strip()
+            if not title:
+                errors['title'] = u"Назва є обов'язковою."
+            else:
+                data['title'] = title
+
+            teacher = request.POST.get('teacher', '').strip()
+            if not teacher:
+                errors['teacher'] = u"Викладач є обов’язковим"
+            else:
+                data['teacher'] = teacher
+
+            date_exam = request.POST.get('date_exam', '').strip()
+            if not date_exam:
+                errors['date_exam'] = u"Дата екзамену є обов’язковою"
+            else:
+                try:
+                    datetime.strptime(date_exam, '%Y-%m-%d %H:%M:%S')
+                except Exception:
+                    errors['date_exam'] ='Введіть коректний формат дати(напр.1984-12-30 12:30:00)'
+                else:
+                    data['date_exam'] = date_exam
+
+            if not errors:
+                exam = Exam(**data)
+                exam.save()
+                messages.success(request, u'Екзамен %s успішно додано!' %(data['title']))
+                return HttpResponseRedirect(reverse('exams'))
+            else:
+                # render form with errors and previous user input
+                messages.error(request,u'Виправте наступні помилки!')
+                return render(request, 'students/exams_add.html',
+                    {'groups': Group.objects.all().order_by('title'),'errors': errors})
+        elif request.POST.get('cancel_button') is not None:
+            # redirect to home page on cancel button
+            messages.warning(request,u'Додавання екзамну скасовано!')
+            return HttpResponseRedirect(reverse('exams'))
+    else:
+        # initial form render
+        return render(request, 'students/exams_add.html',
+                        {'groups': Group.objects.all().order_by('title')})
 
 
 
+def exams_edit_hand(request, eid):
+    exams = Exam.objects.filter(pk=eid)
+    groups = Group.objects.all()
 
-def exams_edit(request, eid):
-	return HttpResponse('<h1>Edit Exam %s</h1>' %eid)
+    if len(exams) !=1:
+        messages.error(request, 'Обраного екзамену не існує')
+        return HttpResponseRedirect(reverse('exams'))
+    else:
+        if request.method == "POST":
+            exam = Exam.objects.get(pk=eid)
+            if request.POST.get('save_button') is not None:
+
+                errors = {}
+                title = request.POST.get('title', '').strip()
+                if not title:
+                    errors['title'] = u"Імʼя є обовʼязковим."
+                else:
+                    exam.title = title
+
+
+                date_exam = request.POST.get('date_exam', '').strip()
+                if not date_exam:
+                        errors['date_exam'] = u"Дата екзамену є обов’язковою"
+                else:
+                    try:
+                        datetime.strptime(date_exam, '%Y-%m-%d %H:%M:%S')
+                    except Exception:
+                        errors['date_exam'] ='Введіть коректний формат дати(напр.1984-12-30 12:30:00)'
+                    else:
+                        exam.date_exam = date_exam
+
+                teacher = request.POST.get('teacher', '').strip()
+                if not teacher:
+                    errors['teacher'] = u"Викладач є обов’язковим"
+                else:
+                    exam.teacher = teacher
+
+
+                group = request.POST.get('group', '').strip()
+                if not group:
+                    errors['group'] = u"Група є обовʼязковою"
+                else:
+                    group = Group.objects.filter(pk=int(group))
+                    if len(group) != 1:
+                        errors['student_group'] = u"Оберіть коректну групу"
+                    else:
+                        exam.group = group[0]
+
+
+                if errors:
+                    messages.error(request,u'Виправте наступні помилки!')
+                    return render(request, 'students/exams_edit_hand.html',
+                                  {'groups': groups,'errors': errors,'exam':exam , 'pk':eid})
+                else:
+                    exam.save()
+                    messages.success(request, u'Екзамен успішно збереженно!')
+                    return HttpResponseRedirect(reverse('exams'))
+
+            elif request.POST.get('cancel_button') is not None:
+                messages.warning(request, 'Редагування екзамену скасовано.')
+                return HttpResponseRedirect(reverse('exams'))
+            else:
+                messages.error(request, 'Упс! Щось пішло не так. Повторіть спробу пізніше.')
+                return HttpResponseRedirect(reverse('exams'))
+        else:
+            return render(request,'students/exams_edit_hand.html',{'pk': eid, 'exam': exams[0],
+                                                                   'groups': groups.order_by('title')})
 
 class ExamsUpdateForm(ModelForm):
     class Meta:
